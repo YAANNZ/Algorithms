7fd9592ec70f400d1fd8763c04c1801f6ada88c0
ZYYNN <zhuynchn@163.com>
2017-08-31 18:33:53 +0800

    md5

--- Algorithms/ViewController.m
+++ Algorithms/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @interface ViewController ()
 
@@ -14,9 +15,24 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
++ (NSString *)md5WithString:(NSString *)input
+{
+    const char *cStr = [input UTF8String];
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    
+    CC_MD5(cStr, (CC_LONG)strlen(cStr), digest);
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    return  output;
 }
 
 
