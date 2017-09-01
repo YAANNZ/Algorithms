c5408ef54242c258b0e565cb4d61e9db2bafe614
ZYYNN <zhuynchn@163.com>
2017-09-01 17:22:33 +0800

    md5、base64

--- Algorithms/ViewController.m
+++ Algorithms/ViewController.m
@@ -18,9 +18,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+
 }
 
+#pragma mark - md5
 + (NSString *)md5WithString:(NSString *)input
 {
     const char *cStr = [input UTF8String];
@@ -34,6 +35,28 @@
     }
     return  output;
 }
+
+#pragma mark - Base64
+/*
+ Base64是一种数据编码方式，目的是让数据符合传输协议的要求。标准Base64编码解码无需额外信息即完全可逆，即使你自己自定义字符集设计一种类Base64的编码方式用于数据加密，在多数场景下也较容易破解。
+ 
+ 对于数据加密应该使用专门的目前还没有有效方式快速破解的加密算法。比如：对称加密算法AES-128-CBC，对称加密需要密钥，只要密钥没有泄露，通常难以破解；也可以使用非对称加密算法，如 RSA，利用极大整数因数分解的计算量极大这一特点，使得使用公钥加密的数据，只有使用私钥才能快速解密。
+ 
+ 对于数据校验，也应该使用专门的消息认证码生成算法，如 HMAC - 一种使用单向散列函数构造消息认证码的方法，其过程是不可逆的、唯一确定的，并且使用密钥来生成认证码，其目的是防止数据在传输过程中被篡改或伪造。将原始数据与认证码一起传输，数据接收端将原始数据使用相同密钥和相同算法再次生成认证码，与原有认证码进行比对，校验数据的合法性。
+ */
+- (NSString *)base64EncodedStringWithString:(NSString *)str;
+{
+    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
+    return [data base64EncodedStringWithOptions:0];
+}
+
+- (NSString *)base64DecodedStringWithString:(NSString *)Base64Str
+{
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:Base64Str options:0];
+    return [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+}
+
+
 
 
 - (void)didReceiveMemoryWarning {
