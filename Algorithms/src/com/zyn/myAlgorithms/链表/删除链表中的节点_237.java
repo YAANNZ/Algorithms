68bbed50f22b2a826339587a87666579397b325f
YAANNZ <zhuynchn@163.com>
2021-02-24 16:33:43 +0800

    反转一个链表

--- Algorithms/src/com/zyn/myAlgorithms/链表/删除链表中的节点_237.java
+++ Algorithms/src/com/zyn/myAlgorithms/链表/删除链表中的节点_237.java
@@ -2,12 +2,6 @@ package com.zyn.myAlgorithms.链表;
 
 public class 删除链表中的节点_237 {
     // 删除链表中的节点
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-    }
-
     class Solution {
         public void deleteNode(ListNode node) {
             node.val = node.next.val;
---
+++ Algorithms/src/com/zyn/myAlgorithms/链表/反转一个链表_206.java
@@ -0,0 +1,14 @@
+package com.zyn.myAlgorithms.链表;
+
+public class 反转一个链表_206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
