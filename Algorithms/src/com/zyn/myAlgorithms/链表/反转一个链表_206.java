1854d02e87187ab0c7551b774f378f9b9a91ee3d
YAANNZ <zhuynchn@163.com>
2021-02-24 22:09:51 +0800

    反转链表迭代法

--- Algorithms/src/com/zyn/myAlgorithms/链表/反转一个链表_206.java
+++ Algorithms/src/com/zyn/myAlgorithms/链表/反转一个链表_206.java
@@ -11,4 +11,19 @@ public class 反转一个链表_206 {
         head.next = null;
         return newHead;
     }
+
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode tempNode = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = tempNode;
+        }
+        return newHead;
+    }
 }
